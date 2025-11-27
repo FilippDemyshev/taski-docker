dc34d50654d0636d4d8b365a30cd20bb1c6377ab
Демышев Филипп <fil30112000@yandex.ru>
2025-11-27 16:21:36 +0300

    3.0

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'philhost.hopto.org', '89.169.187.4']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 'philhost.hopto.org', '89.169.187.4']
 
 
 # Application definition
@@ -66,7 +67,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-	'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
